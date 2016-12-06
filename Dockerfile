01bcce4bfab40922623598ff304d84eb28d37adc
Joris De Winne <jdewinne@localhost.localdomain>
2016-12-06 13:35:47 -0800

    Adding option for running pytest

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM xebialabs/xld_dev_compile:v6.0.1.1
 
 MAINTAINER Joris De Winne <jdewinne@xebialabs.com>
-ENV REFRESHED_AT 2016-11-23
+ENV REFRESHED_AT 2016-12-06
 
 ADD resources/supervisord.conf /etc/supervisord.conf
 ADD resources/command.sh /opt/xld/command.sh
+ADD resources/xld_test.py /opt/xld/xld_test.py
+ADD resources/jython-standalone-2.7.0.jar /opt/xld/cli/lib/jython-standalone-2.7.0.jar
 
 CMD ["/usr/bin/supervisord"]
--- resources/command.sh
+++ resources/command.sh
@@ -37,6 +37,15 @@ do
             exit $res
           fi
       fi
+      files=(/data/build/resources/test/docker/tests/*)
+      if [ ${#files[@]} -gt 0 ]; then
+          cp /data/build/resources/test/docker/tests/* /opt/xld/cli/ext
+          /opt/xld/cli/bin/cli.sh -username admin -password admin -f /opt/xld/xld_test.py
+          res=$?
+          if [ $res != 0 ] ; then
+            exit $res
+          fi
+      fi
       sleep 4
       exit
     fi
