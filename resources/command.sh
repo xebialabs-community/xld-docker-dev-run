a8f19620f5349ee7ff7590afd18ce776ed4aebe5
Joris De Winne <joris.dewinne@gmail.com>
2018-07-17 16:51:40 -0700

    Making sure xld starts

--- resources/command.sh
+++ resources/command.sh
@@ -2,11 +2,15 @@
 
 # Copy downloads into 'plugins'
 if [ -d "/data/build/downloads/plugins" ]; then
-    cp /data/build/downloads/plugins/* /opt/xl-deploy-server/plugins
+    cp /data/build/downloads/plugins/* /opt/xl-deploy-server/default-plugins
 fi
 
 # Copy to plugins
-cp /data/build/distributions/*.xldp /opt/xl-deploy-server/plugins
+cp /data/build/distributions/*.xldp /opt/xl-deploy-server/default-plugins
 
 # Link `ext` folder
 find /data/src/main/resources -maxdepth 1 -mindepth 1 -type d -exec ln -s -f '{}' /opt/xl-deploy-server/ext/ \;
+
+export ADMIN_PASSWORD=admin
+export REPOSITORY_KEYSTORE_PASSPHRASE=admin
+/usr/bin/dumb-init /opt/xl-deploy-server/bin/run-in-container.sh
