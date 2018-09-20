1c02720af6d446fa4855fd65cfd7d06be44aeb2a
Joris De Winne <joris.dewinne@gmail.com>
2018-09-20 11:56:42 -0700

    Adding link to XLD license

--- resources/command.sh
+++ resources/command.sh
@@ -11,6 +11,9 @@ cp /data/build/distributions/*.xldp ${APP_HOME}/default-plugins
 # Link `ext` folder
 find /data/src/main/resources -maxdepth 1 -mindepth 1 -type d -exec ln -s -f '{}' ${APP_HOME}/ext/ \;
 
+# Link license
+ln -s /license/deployit-license.lic ${APP_HOME}/default-conf/deployit-license.lic
+
 export ADMIN_PASSWORD=admin
 export REPOSITORY_KEYSTORE_PASSPHRASE=admin
 /opt/xebialabs/tini ${APP_HOME}/bin/run-in-container.sh
