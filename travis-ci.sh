8de4cbe610836e4fbe82bbec7d56ad6cf33ed4a4
Rick Broker <rick.broker@gmail.com>
2020-01-31 12:53:07 -0500

    updater keys

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d "
+{
+\"username\": \"$DOCKER_USERNAME\",
+\"password\": \"$DOCKER_PASSWORD\"
+}
+" https://hub.docker.com/v2/users/login/ | jq -r .token)
+
 http --follow https://gist.githubusercontent.com/jdewinne/3f13494858fad8b6b2b88ebd3439f1ea/raw/dce2f06fea89a6038acd6944c516c8be9347c48d/dockertags > ./dockertags
 chmod +x dockertags
 
 image_name="xebialabsunsupported/xld_dev_run"
-./dockertags -i xebialabsunsupported/xld_dev_compile > /tmp/xld_dev_compile
-./dockertags -i $image_name > /tmp/xld_dev_run
+./dockertags -i xebialabsunsupported/xl-deploy -e "centos$" > /tmp/xld
+./dockertags -i $image_name > /tmp/xl
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 while read tag ; do
     docker build -t $image_name:$tag --build-arg xld_tag=$tag .
+    echo "####################################################################"
+    echo "####        $image_name:$tag built"
     docker push $image_name:$tag
-done < <(comm -23 <(sort /tmp/xld_dev_compile) <(sort /tmp/xld_dev_run))
+    echo "####        $image_name:$tag pushed"
+    echo "####################################################################"
+done < <(comm -23 <(sort /tmp/xld) <(sort /tmp/xl))
