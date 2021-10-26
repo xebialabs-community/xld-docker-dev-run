b4b209610c806742d07751651456a59bc67c787b
zvercodebender <rbroker@digital.ai>
2021-10-26 10:20:33 -0400

    cat dockertags

--- travis-ci.sh
+++ travis-ci.sh
@@ -10,6 +10,7 @@ TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d "
 
 http --follow https://gist.githubusercontent.com/jdewinne/3f13494858fad8b6b2b88ebd3439f1ea/raw/dce2f06fea89a6038acd6944c516c8be9347c48d/dockertags > ./dockertags
 chmod +x dockertags
+cat ./dockertags
 
 image_name="xebialabsunsupported/xld_dev_run"
 bash ./dockertags -i xebialabsunsupported/xl-deploy -e "centos$" > /tmp/xld
